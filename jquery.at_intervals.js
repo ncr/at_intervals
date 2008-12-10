6598e12b964ab1cb287f8b9c2102692a112e420d
Jacek Becela <jacek.becela@gmail.com>
2008-12-10 21:06:49 +0100

    First release! Yay!

---
+++ jquery.at_intervals.js
@@ -0,0 +1,15 @@
+(function($) {
+  $.fn.at_intervals = function(codes, interval) {
+    return this.each(function() {
+      var it = this // this is it :)
+      var interval_id = setInterval(function() {
+        // check if "it" is still in the DOM
+        if($(it).parents("html").length == 1) {
+          codes();
+        } else {
+          clearInterval(interval_id);
+        }
+      }, interval);
+    });
+  };
+})(jQuery);
