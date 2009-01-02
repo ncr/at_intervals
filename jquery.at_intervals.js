c63e64812566433800f0c3d9cb8115da1c1d2b68
Jacek Becela <jacek.becela@gmail.com>
2009-01-02 13:05:24 +0100

    Added ability to pause/resume and stop intervals from outside

--- jquery.at_intervals.js
+++ jquery.at_intervals.js
@@ -1,17 +1,49 @@
 // Author: Jacek Becela
 // License: cc-by-sa
 (function($) {
-  $.fn.at_intervals = function(codes, interval) {
+  $.fn.at_intervals = function(fn, options) { // name and interval are mandatory
+    
     return this.each(function() {
-      var it = this // this is it :)
-      var interval_id = setInterval(function() {
-        // check if "it" is still in the DOM
-        if($(it).parents("html").length == 1) {
-          codes();
-        } else {
-          clearInterval(interval_id);
+      var it = $(this) // capturing this
+      var name = options.name
+      var delay = options.delay
+
+      var helper = {
+        should_stop: function() { // used to completely remove the interval
+          return this.removed_from_dom() || this.user_wants_to_stop()
+        },
+        should_work: function() { // used to pause/resume the interval
+          return this.is_visible() && !this.user_wants_to_pause()
+        },
+        removed_from_dom: function() {
+          return $(it).parents("html").length == 0
+        },
+        user_wants_to_stop: function() {
+          return it.data(name).should_stop == true
+        },
+        is_visible: function() {
+          return it.parents("*").andSelf().not(":visible").length == 0
+        },
+        user_wants_to_pause: function() {
+          return it.data(name).should_pause == true
         }
-      }, interval);
-    });
+      }
+      
+      var interval_id = setInterval(function() {
+        if(helper.should_stop()) {
+          clearInterval(interval_id)
+          it.removeData(name)
+        } else {
+          if(helper.should_work()){
+            fn()
+          }
+        }
+      }, delay)
+      
+      it.data(name, { 
+        delay: delay,
+        interval_id: interval_id
+      })
+    })
   };
 })(jQuery);
